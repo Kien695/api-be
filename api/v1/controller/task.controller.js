36ceebd7dd38658d1c7f7d02fbd90117a8fe0dec
kien <dp1.1a2kien@gmail.com>
2024-12-01 16:30:58 +0700

    bộ lọc

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -1,9 +1,13 @@
 const Task = require("../models/task.model");
 //[get] /api/v1/task
 module.exports.index = async (req, res) => {
-  const tasks = await Task.find({
+  const find = {
     deleted: false,
-  });
+  };
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  const tasks = await Task.find(find);
   res.json(tasks);
 };
 //[get]/api/vi/task/detail/:id
