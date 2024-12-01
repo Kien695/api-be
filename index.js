f83ccee0f01e9cbf4c3aa3d6ac5b0da5f10fcab7
kien <dp1.1a2kien@gmail.com>
2024-12-01 18:49:57 +0700

    change

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const database = require("./config/database");
 const router = require("./api/v1/router/index.router");
 database.connect();
 const app = express();
 const port = process.env.PORT;
+// parse application/json
+app.use(bodyParser.json());
+
 router(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
