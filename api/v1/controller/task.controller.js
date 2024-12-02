7c7ae4d59b43276c7c80dd20b50490289aae4b58
kien <dp1.1a2kien@gmail.com>
2024-12-02 17:56:26 +0700

    delete

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -151,3 +151,27 @@ module.exports.edit = async (req, res) => {
     });
   }
 };
+//[patch]]/api/vi/task/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Task.updateOne(
+      {
+        _id: id,
+      },
+      {
+        deleted: true,
+        deletedAt: new Date(),
+      }
+    );
+    res.json({
+      code: 200,
+      message: "Xóa thành công",
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi!",
+    });
+  }
+};
