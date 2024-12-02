6903792b86906f7c65f29ed341d8281b0bc5456e
kien <dp1.1a2kien@gmail.com>
2024-12-02 17:16:29 +0700

    đổi nhiều

--- api/v1/router/task.router.js
+++ api/v1/router/task.router.js
@@ -4,4 +4,5 @@ const controller = require("../controller/task.controller");
 router.get("/", controller.index);
 router.get("/detail/:id", controller.detail);
 router.patch("/change-status/:id", controller.changeStatus);
+router.patch("/change-multi", controller.changeMulti);
 module.exports = router;
