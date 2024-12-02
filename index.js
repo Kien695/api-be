26f9ee5183ab8be3d47ae80289f4338c0afdebc6
kien <dp1.1a2kien@gmail.com>
2024-12-02 20:12:38 +0700

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 const database = require("./config/database");
 const router = require("./api/v1/router/index.router");
 database.connect();
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
 
