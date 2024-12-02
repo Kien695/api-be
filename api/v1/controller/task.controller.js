4ec3fd07a643d685edc7bbad4978e89f84cc95b9
kien <dp1.1a2kien@gmail.com>
2024-12-02 17:31:32 +0700

    create

--- api/v1/controller/task.controller.js
+++ api/v1/controller/task.controller.js
@@ -113,3 +113,20 @@ module.exports.changeMulti = async (req, res) => {
     });
   }
 };
+//[post]/api/vi/task/create
+module.exports.create = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const data = await task.save();
+    res.json({
+      code: 200,
+      message: "Tạo thành công",
+      data: data,
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Lỗi!",
+    });
+  }
+};
